f7dad500e8d879e00a42eff4c79ef3a9a74ca98c
fatimahazzahra015 <ftmhzhr016@gmail.com>
2026-01-30 22:56:44 +0700

    vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_PATH || '/Berita-Kini',
+  base: '/', 
   server: {
     proxy: {
       '/api-news': {
